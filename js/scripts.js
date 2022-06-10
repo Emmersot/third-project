51bf81db38e835bf5a2c0a333297a14c68df5bcb
Tyler Emmerson <TylerEmmerson0216@gmail.com>
2022-06-10 11:50:09 -0700

    add html formatting, continue to produce function with HTML attributes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
-$(document).ready(function() {
-const input = prompt("Enter a sentence");
-  console.log(input);
-});
+
 //1st function will take a range of numbers inputted by the user and return 1's as "Beep!"
 
+const input = ("input")
 
 
 
 
 
-
-function beepBoop(input) {
+function beepBoopFunction(element) {
 
 for (var i = 0; i <= input + 1; i + 1 ) {
-  if(i === 3) console.log("Won't you be my neighbor?");
-                // return ("Won't you be my neighbor?");
-  else if (i === 2) console.log("Boop!");
-                // return ("Boop!"); 
-  else if (i === 1) console.log("Beep!");
-                // return ("Beep!");
-  else console.log(i);
+  if(i === 3) {
+      // console.log("Won't you be my neighbor?");
+                print("Won't you be my neighbor?");
+    } else if (i === 2) {
+      // console.log("Boop!");
+                print("Boop!"); 
+    } else if (i === 1) {
+      // console.log("Beep!");
+                print("Beep!");
+    } else { 
+      $("#output").text(result);
+    }
   }
 };
+
+// User interface logic
+
+$(document).ready(function(event) {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    const beepBoop = parseint($("input").val());
+    const result = beepBoop
+    });
+    $("#output").text(result);
+});
